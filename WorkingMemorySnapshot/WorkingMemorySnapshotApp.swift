f2e5c5fb1335ca66d15940e71ea3fc2c7e5b2d1e
Zachary Kissinger <zacharypkissinger@gmail.com>
2026-06-26 11:30:22 -0600

    chore: add Xcode app scaffold

---
+++ WorkingMemorySnapshot/WorkingMemorySnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingMemorySnapshotApp.swift
+//  WorkingMemorySnapshot
+//
+//  Created by Zachary Kissinger on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct WorkingMemorySnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
